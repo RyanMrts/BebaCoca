4fd9977f0db339a9365f7ac8f7fd2fdb58a11495
RyanMrts <ryanmartinssilva2006@gmail.com>
2023-03-11 12:50:23 -0300

    exercicio finalizado

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("beba  coca  cola   ");
+Console.WriteLine("babe           cola");
+Console.WriteLine("beba  coca"         );
+Console.WriteLine("babe  cola  caco   ");
+Console.WriteLine("caco               ");
+Console.WriteLine("cola               ");
+Console.WriteLine("        c l o a c a");
+Console.ResetColor();
